654302dc03791d5c35444873bfb37c6151434b9a
耿贤坤 <gengxiankun@vn.com>
2024-09-09 21:32:51 +0800

    feat(/): init

---
+++ src/main/java/com/holdingkey/component/result/ResultCodeInterface.java
@@ -0,0 +1,12 @@
+package com.holdingkey.component.result;
+
+/**
+ * 状态码接口
+ * @author xiankun.geng
+ */
+public interface ResultCodeInterface {
+
+    String getCode();
+
+    String getMessage();
+}
